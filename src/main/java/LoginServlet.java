450e24752e686ebee4dd5f488ff3c0e5f7643190
ajinkya kambe <ajinkyakambe@gmail.com>
2022-06-15 17:35:04 +0530

    UC3 and UC4: Added

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -14,20 +14,38 @@ with predefined user
         urlPatterns={"/LoginServlet"},
         initParams = {
                 @WebInitParam(name = "user",value = "ajinkya"),
-                @WebInitParam(name = "password",value = "kambe")
+                @WebInitParam(name = "password",value = "kambe@2007")
                 }
 )
 
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //get request parameters for user ID and password
         String user= request.getParameter("user");
         String pwd= request.getParameter("pwd");
-
+        // get servlet config init params
         String userId = getServletConfig().getInitParameter("user");
         String password= getServletConfig().getInitParameter("password");
 
-        if (userId.equals(user) && password.equals(pwd)){
+        /* UC3:
+        Extend the Servlet to accept a valid Name
+        - Name starts with Cap and has minimum 3 characters
+         */
+
+        String nameValidate = "^[A-Z][a-z]{5}";
+
+        /*UC 4
+        Extend the Servlet to accept a valid Password
+        - Rule1 – minimum 8 Characters
+        - Rule2 – Should have at least 1 Upper Case
+        - Rule3 – Should have at least 1 numeric number in the password
+        - Rule4 – Has exactly 1 Special Character
+         */
+        String passwordValidate = "^(?=.*[0-9])(?=[^@#$%^&+=]*[@#$%^&+=][^@#$%^&+=]*$)(?=.*[a-z])(?=.*[A-Z]).{8,}$";
+
+
+        if(userId.equals(user) && userId.matches(nameValidate) && password.equals(pwd) && password.matches(passwordValidate)) {
             request.setAttribute("user",user);
             request.getRequestDispatcher("LoginSuccess.jsp").forward(request,response);
         }else {
